6eef81897991916387f39b4b479f8c33bd32f0cf
PabloAmico <70479504+PabloAmico@users.noreply.github.com>
2021-03-18 08:39:48 -0300

    Agregamos todo a git, retomando el proyecto luego de 4 meses

--- Proyecto_Final/Recursos/Scripts/zombie.gd
+++ Proyecto_Final/Recursos/Scripts/zombie.gd
@@ -1,21 +1,31 @@
 extends KinematicBody2D
 
 var Vida = 3
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var Estado_Actual
+var Estado_Anterior
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
 func Quitar_Vida():
 	Vida = Vida - 1
 	print(Vida)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(Vida <= 0):
 		queue_free()
 	pass
+
+func _physics_process(delta):
+	pass
+
+func Cambiar_Estado(Nuevo_Estado):
+	Estado_Anterior = Estado_Actual
+	Estado_Actual = Nuevo_Estado
+	print("Se cambio de estado")
+
+func Estado_Idle():
+	$AnimacionZombie.play("Zombie_Idle")
+
+func _on_Timer_Cambio_Estado_timeout():
+	pass # Replace with function body.
